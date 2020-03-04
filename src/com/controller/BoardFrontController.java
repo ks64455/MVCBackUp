98bfcde52dc991ec39207fec617e80805787fa8a
jungseo <pealyteill@naver.com>
2020-03-04 09:31:05 +0900

    박정서

--- src/com/controller/BoardFrontController.java
+++ src/com/controller/BoardFrontController.java
@@ -11,25 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.service.BoardCommand;
 import com.service.BoardListCommand;
+import com.service.BoardpwdCheckCommand;
+import com.service.BoardpwdCheckFormCommand;
 
 /**
  * Servlet implementation class BoardFrontController
  */
 @WebServlet("*.do")
 public class BoardFrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoardFrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doPost(request,response);
@@ -51,6 +40,30 @@ public class BoardFrontController extends HttpServlet {
 			command = new BoardListCommand();
 			command.execute(request, response);
 			nextPage = "list.jsp";
+		}
+		// 비밀번호 입력 화면
+		if(com.equals("/pwdCheckui.do")) {
+			command = new BoardpwdCheckFormCommand();
+			command.execute(request, response);
+			nextPage = "passwdChk.jsp";
+		}
+		// 비밀번호 체크
+		if(com.equals("/pwdCheck.do")) {
+			command = new BoardpwdCheckCommand();
+			command.execute(request, response);
+			nextPage = (String)request.getAttribute("resultUrl");
+		}
+		// 글 수정 화면 보기
+		if(com.equals("/updateui.do")) {
+			command = new BoardRetrieveCommand();
+			command.execute(request, response);
+			nextPage = "update.jsp";
+		}
+		// 글 수정 하기
+		if(com.equals("/update.do")) {
+			command = new BoardUpdateCommand();
+			command.execute(request, response);
+			nextPage = "list.do";
 		}
 		RequestDispatcher dis = request.getRequestDispatcher(nextPage);
 		dis.forward(request, response);
